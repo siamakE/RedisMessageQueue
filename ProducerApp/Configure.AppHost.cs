b5e1b5708b73df1c27f58e40825006f3807f565f
Siamak <Esmaeili_cs@outlook.com>
2022-06-18 16:21:50 +0430

    refactor

--- ProducerApp/Configure.AppHost.cs
+++ ProducerApp/Configure.AppHost.cs
@@ -2,6 +2,8 @@ using Funq;
 using ServiceStack;
 using ProducerApp.ServiceInterface;
 using ServiceStack.Redis;
+using ServiceStack.Messaging;
+using ServiceStack.Messaging.Redis;
 
 [assembly: HostingStartup(typeof(AppHost))]
 
@@ -11,14 +13,17 @@ public class AppHost : AppHostBase, IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
         .ConfigureServices(services => {
-            //_ = services.AddSingleton<IRedisClientsManager>(new RedisManagerPool("localhost:6379"));
+            var mqServer = new RedisMqServer(new RedisManagerPool("localhost:6379"))
+            {
+                DisablePublishingToOutq = true,
+            };
+            services.AddSingleton<IMessageService> (mqServer);
         });
 
     public AppHost() : base("ProducerApp", typeof(MyServices).Assembly) {}
 
     public override void Configure(Container container)
     {
-        // Configure ServiceStack only IOC, Config & Plugins
         SetConfig(new HostConfig {
             UseSameSiteCookies = true,
         });
--- ProducerApp/MyServices.cs
+++ ProducerApp/MyServices.cs
@@ -8,26 +8,9 @@ namespace ProducerApp.ServiceInterface;
 
 public class MyServices : Service
 {
-    private IRedisClientsManager redisManager;
-    private IMessageFactory MqFactory;
     public object Any(Hello request)
     {
-        redisManager = new RedisManagerPool("localhost:6379");
-        MqFactory = new RedisTransientMessageFactory(redisManager);
-
-        using (IMessageQueueClient mqClient = MqFactory.CreateMessageQueueClient())
-        {
-            //var replyToMq = mqClient.GetTempQueueName();
-
-            mqClient.Publish(new Message<Hello>(new Hello { Name = "MQ Worker" }));
-            //{
-            //    ReplyTo = replyToMq,
-            //});
-
-            //var responseMsg = mqClient.Get<HelloResponse>(replyToMq);
-            //mqClient.Ack(responseMsg);
-        }
-
+        PublishMessage(request);
         return new HelloResponse { Result = $"Hello, {request.Name}!" };
     }
 }
